e1d877abf43fb3c76fed66729ffa35c45eae1fdc
ar5hi <114194077+ar5hi@users.noreply.github.com>
2023-05-09 18:00:34 +0530

    Update App.js

--- App.js
+++ App.js
@@ -1,29 +1,10 @@
-import React, {useState} from 'react'
-import List from './to-do project/List'
-import MemoHook from './MemoHook'
-import Parent from './Parent'
-import ReducerHook from './ReducerHook'
-import RefHook from './RefHook'
-import CallbackHook from './callback/CallbackHook'
-import Main from './stateMgmt/Main'
-import Stopwatch from './Stopwatch'
-import Reversetodo from './Reversetodo'
-import Test from './Test'
+import React from 'react'
 
 function App() {
   
+import Test from './Test'
   return (
     <div>
-      App
-          {/* <Parent/>
-          <MemoHook/>
-          <RefHook/>
-          <ReducerHook/>
-          <List />
-          <CallbackHook/> */}
-      {/* <Main/> */}
-      {/* <Stopwatch/> */}
-      {/* <Reversetodo/> */}
       <Test/>
     </div>
   )
